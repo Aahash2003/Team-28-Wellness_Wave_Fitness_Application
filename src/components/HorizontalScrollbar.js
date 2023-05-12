70a6fe4810c38e6ee31f0df4812a2f30b9ba9ead
Aahash2003 <Aahash2003.saahash@gmail.com>
2023-05-12 03:41:42 -0400

    SearchExer

---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HorizontlaScrollbar = () => {
+  return (
+    <div>HorizontlaScrollbar</div>
+  )
+}
+
+export default HorizontlaScrollbar
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,13 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.NODE_ENV
+        // fix it within the env class
+
+    }
+};
+
 export const fetchData = async( url, options) => {
 const response = await fetch(url, options);
 const data = await response.json();
