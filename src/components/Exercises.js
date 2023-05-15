3e1c484c704dee8a156ec587a755e8d1dc2c709b
Aahash2003 <Aahash2003.saahash@gmail.com>
2023-05-15 04:05:20 -0400

    Finished9forCue

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,7 +7,22 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-  console.log(exercises);
+
+  const[currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage= 9;
+
+  const indexOfLastEx= currentPage*exercisesPerPage;
+  const indexOfFirstEx= indexOfLastEx-exercisesPerPage;
+  const currentEx= exercises.slice(indexOfFirstEx, indexOfLastEx);
+
+
+  const paginate = (e, value ) => {
+    setCurrentPage(value);
+
+    window.scrollTo({top:1800, behavior: 'smooth'})
+
+  }
+
   return (
     <Box id="exercises"
       sx={{ mt: { jg: '110px' } }}
@@ -20,9 +35,23 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       </Typography>
       <Stack direction="row" sx={{ gap: { lg: '110px', xs: '5opx' } }}
         flexWrap="wrap" justifyContent="center">
-        {exercises.map((exercise, index) => (
+        {currentEx.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+          color="standard"
+          shape="rounded"
+          defaultPage={1}
+          count={Math.ceil(exercises.length / exercisesPerPage)}
+          page={currentPage}
+          onChange={paginate}
+          //materialUI for info on this
+          />
+        )}
 
       </Stack>
     </Box>
