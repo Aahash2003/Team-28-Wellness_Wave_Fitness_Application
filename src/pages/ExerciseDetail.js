a24ef29c71d3adbaae8d92f152918fbadd865abd
Aahash2003 <Aahash2003.saahash@gmail.com>
2023-05-15 20:05:32 -0400

    FIXEQUIPNAME

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail]= useState({});
+  const [exerciseVideos, setexerciseVideos]= useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      console.log({exerciseDetailData});
+
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=
+      ${exerciseDetailData.name}`, youtubeOptions);
+      setexerciseVideos(exerciseVideosData);
+
+    }
+
+    fetchExercisesData();
+  }, [id]);
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail= {exerciseDetail}/>
+
+      <ExerciseVideos exerciseVideos= {exerciseVideos} name = {exerciseDetail.name}/>
+
+      <SimilarExercises/>
+
+    </Box>
   )
 }
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -8,6 +8,14 @@ export const exerciseOptions = {
     }
 };
 
+export const youtubeOptions = {
+    method: 'GET',
+   
+    headers: {
+      'X-RapidAPI-Key': '6b14f01acamshaab86d1944a63d1p1fd522jsn040a6d9bf0a0',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
 export const fetchData = async( url, options) => {
 const response = await fetch(url, options);
 const data = await response.json();
