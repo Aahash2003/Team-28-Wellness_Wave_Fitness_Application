255f7dfd9df79fcf7110dfecb704f7b38d16432b
Aahash2003 <Aahash2003.saahash@gmail.com>
2023-05-19 05:21:24 -0400

    DONE

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
   return (
     <Box mt="80x" bgcolor="#fff3f4">
       <Stack gap="40px" 
-      alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="logo" width="200px" height="40px"/>
-        <Typography variant="h5" pb="40px" mt="40px">
+      alignItems="center" px="0px" pt="24px">
+        <img src={Logo} alt="logo" width="800px" height="160px"/>
+        <Typography variant="h5" pb="40px" mt="0px">
           By Aahash Srikumar :)
         </Typography>
       </Stack>
