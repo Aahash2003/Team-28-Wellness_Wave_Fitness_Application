52689ee61c20cecc7c1915efd50cbb46c8140c5e
Aahash2003 <Aahash2003.saahash@gmail.com>
2023-05-22 14:19:34 -0400

    fixxxxxed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        'X-RapidAPI-Key': '9beeaabc65msh7943379eee53d21p11afc2jsna85799c2c2c2'
+        'X-RapidAPI-Key': '8a728a8cd5msh78882717985ecb6p1a2723jsnb943d0ac0fc8'
         // fix it within the env class
 
     }
