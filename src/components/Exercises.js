2c9d72f60dbef70f90dc584bd3a9217a366bd761
Aahash2003 <Aahash2003.saahash@gmail.com>
2024-07-11 00:10:26 -0400

    Search Bar Limitation (Better Results)

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -28,10 +28,10 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       let exercisesData = [];
 
       if(bodyPart === 'all') {
-        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=100', exerciseOptions);
       }
       else{
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=100`, exerciseOptions);
       }
 
       setExercises(exercisesData);
