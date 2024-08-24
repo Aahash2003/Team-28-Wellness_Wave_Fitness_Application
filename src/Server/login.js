c117d7b72c4692659d8c1c486ae42639a82a8187
Aahash2003 <Aahash2003.saahash@gmail.com>
2024-08-24 16:45:19 -0400

    Updating Port IP Access

--- src/Server/login.js
+++ src/Server/login.js
@@ -27,4 +27,4 @@ app.use("/api/calc", calcRoutes)
 app.use("/api/profile", profileRoutes)
 
 const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}...`));
